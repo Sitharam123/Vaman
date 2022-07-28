f3f81e6a5173df7edd996025e6093b0d27a94110
Sitharam123 <110094194+Sitharam123@users.noreply.github.com>
2022-07-28 20:15:05 +0530

    Add files via upload

---
+++ fpga codes/blink/onoff.v
@@ -0,0 +1,33 @@
+module helloworldfpga(
+    output wire redled,
+    output wire greenled,
+    output wire blueled
+);
+    wire clk;
+
+    qlal4s3b_cell_macro u_qlal4s3b_cell_macro (
+        .Sys_Clk0 (clk),
+    );
+
+    reg[26:0] delay;
+     reg	led;
+
+
+always@(posedge clk) begin
+delay = delay+1; 
+
+if(delay > 20000000) 
+begin
+delay=27'b0;
+led=1; 
+end
+end
+
+
+assign greenled = led; 
+endmodule
+
+
+
+
+
