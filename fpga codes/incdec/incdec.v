eb1e0819e27206eff67abd20600718826cf212a7
Sitharam123 <110094194+Sitharam123@users.noreply.github.com>
2022-07-28 20:19:55 +0530

    Add files via upload

---
+++ fpga codes/incdec/incdec.v
@@ -0,0 +1,49 @@
+module helloworldfpga(
+
+input  wire W,
+input  wire X,
+input  wire Y,
+input  wire Z,
+
+output wire a,
+output wire b,
+output wire c,
+output wire d,
+output wire e,
+output wire f,
+output wire g
+
+);
+
+
+reg A,B,C,D;
+
+always @(*)
+begin
+
+D = (W&X&Y&(!Z))|((!W)&(!X)&(!Y)&Z);
+C=((!Z)&(!Y)&(X)&W)|((!Z)&(Y)&!X&(!W))|((!Z)&Y&(!X)&W)|((!Z)&Y&X&(!W));
+B=((!Z)&(!Y)&!X&W)|((!Z)&!Y&(X)&(!W))|((!Z)&Y&(!X)&W)|((!Z)&Y&X&(!W));
+A = ((!W)&(!X)&(!Y)&(!Z))|((!W)&(X)&(!Y)&(!Z))|((!W)&(!X)&Y&(!Z))|((!W)&X&Y&(!Z))|((!W)&(!X)&(!Y)&(Z));
+
+
+a=(!D&!C&!B&A)|(!D&C&!B&!A);
+b=(!D&C&!B&A)|(!D&C&B&!A);
+c=(!D&!C&B&!A);
+d=(!D&!C&!B&A)|(!D&C&!B&!A)|(!D&C&B&A);
+e=(!D&!C&!B&A)|(!D&!C&B&A)|(!D&C&!B&!A)|(!D&C&!B&A)|(!D&C&B&A)|(D&!C&!B&A);
+f=(!D&!C&!B&A)|(!D&!C&B&!A)|(!D&!C&B&A)|(!D&C&B&A);
+g=(!D&!C&!B&!A)|(!D&!C&!B&A)|(!D&C&B&A);
+
+end
+endmodule
+
+
+
+
+
+
+
+
+
+
